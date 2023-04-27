e0e1cbe390aae2fe6050c0af9ceed0a436aebe2f
Brent Buchholz <brentbuchholz@gmail.com>
2023-04-27 14:12:51 -0500

    Add logo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,18 +16,7 @@ const { t } = useI18n()
 
 <template>
   <div>
-    <img alt="Coventry Grove Builders Association Presents logo" src="/CGBAP_LOGO_CROPPED.png">
-    <p>
-      <a rel="noreferrer" href="https://github.com/BBuchholz/worthy-mountain" target="_blank">
-        Sonic Circle
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>A Place 4 Musicians 2 Play</em>
-    </p>
-
-    <div py-4 />
-
+    <img alt="Sonic Circle logo" src="public/Sonic Circle-logos_transparent.png">
     <TheInput
       v-model="name"
       placeholder="enter myrKi..."
@@ -55,8 +44,8 @@ meta:
 
 <style>
 img {
-  height: 25%;
-  width: 25%;
+  height: 100%;
+  width: 100%;
   display: block;
   margin-left: auto;
   margin-right: auto;
